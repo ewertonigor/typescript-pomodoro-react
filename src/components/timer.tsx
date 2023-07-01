87d5516b9fec4a0d5a4af4726da9a80dcdfde644
Ewerton Igor <ewertonigor@icloud.com>
2023-06-30 21:07:00 -0300

    Componentes do Timer e Button

---
+++ src/components/timer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { secondsToTime } from '../utils/seconds-to-time';
+
+type Props = {
+  mainTime: number;
+};
+
+export function Timer(props: Props): JSX.Element {
+  return <div className="timer">{secondsToTime(props.mainTime)}</div>;
+}
